5e3d3ed0da8a0fb0d06443ed19a77c309c14d1f0
Cassie Achzenick <caachzenick@gmail.com>
2020-03-17 14:51:45 -0600

    Add models and relationships

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-describe Item, type: :model do
+RSpec.describe Item, type: :model do
   describe "validations" do
+     it { should validate_presence_of(:name) }
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price }
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Item, type: :model do
+describe Merchant, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
   end
