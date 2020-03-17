81d90a5cb13621ffb61e78c3eee3c520e533ff99
Cassie Achzenick <caachzenick@gmail.com>
2020-03-16 20:22:19 -0600

    Add model tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,6 +8,7 @@ describe Invoice, type: :model do
   end
 
   describe "relationships" do
+    it {should have_many :transactions}
     it {should belong_to :customer}
     it {should belong_to :merchant}
     it {should have_many :invoice_items}
