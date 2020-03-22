000c1cfa3838c6b13f0cea49233989dc28ca9cbd
Cassie Achzenick <caachzenick@gmail.com>
2020-03-21 18:39:07 -0600

    Add dynamic merchant search

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,8 +1,21 @@
 class Api::V1::Merchants::FindController < ApplicationController
   def show
+    injection = ""
+    values = {}
     find_params.each do |key, value|
-      render json: MerchantSerializer.new(Merchant.where("lower(#{key}) like ?", "%#{value}%").first)
+      string = " lower(#{key}) like :#{key} OR"
+      injection = injection + string
+      values[key.to_sym] = "%#{value}%"
     end
+    injection.slice!" "
+    injection.delete_suffix!(' OR')
+
+    merchant = Merchant.where("#{injection}", values).first
+    render json: MerchantSerializer.new(merchant)
+
+    # find_params.each do |key, value|
+    #   render json: MerchantSerializer.new(Merchant.where("lower(#{key}) like ?", "%#{value}%").first)
+    # end
   end
 
   private
